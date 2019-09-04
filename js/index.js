0bc642bcd10e4a7631a7ef503391592264c6f10e
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-09-04 16:04:23 -0700

    akak-almazbekova

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+
+//Logo text click alert message
+const boatContainer = document.querySelector(".nav-container h1");
+boatContainer.addEventListener("click", event => {
+  alert("Choose your Dream Desitination Today!");
+});
+
+// All H2's -  Mouse Over Color Change
+const h2 = document.querySelectorAll("h2");
+h2.forEach(changeColor => {
+  changeColor.addEventListener("mouseover", event => {
+    changeColor.style.color = "red";
+  });
+});
+
+// All H2's -  Mouse Over Color Change
+const a = document.querySelectorAll(".nav-link");
+a.forEach(resize => {
+  resize.addEventListener("mouseover", event => {
+    resize.style.color = "red";
+  });
+});
+
+// Intro Content - Mouse Enter, Mouse Leave Size Scale
+const introSizeUp = document.querySelectorAll(".intro");
+introSizeUp.forEach(resize => {
+  resize.addEventListener("mouseenter", event => {
+    resize.style.transform = "scale(1.1)";
+    resize.style.transition = " transform 0.5s";
+    resize.style.color = "hotpink";
+  });
+});
+
+const introSizeDown = document.querySelectorAll(".intro");
+introSizeDown.forEach(resize => {
+  resize.addEventListener("mouseleave", event => {
+    resize.style.transform = "scale(1)";
+    resize.style.transition = "transform 0.5s";
+  });
+});
